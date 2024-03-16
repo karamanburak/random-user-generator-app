3efa4f419db879846edeafbe2a26c41013ec0368
burak <karaman.buraak@gmail.com>
2024-03-16 20:50:04 +0100

    Create new useState Hooks

--- src/App.js
+++ src/App.js
@@ -14,15 +14,18 @@ import axios from "axios";
 
 
 function App() {
+
+  const BASE_URL = "https://randomuser.me/api/";
   const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
-  const [people,SetPeople] = useState([])
-  const [loading,setLoading] = useState(true)
-  const BASE_URL = "https://randomuser.me/api/";
+  const [userData,setUserData] = useState(null)
+  const [people,setPeople] = useState([])  
+  const [activeIcon, setActiveIcon] = useState(0)
+  const [loading,setLoading] = useState(false)
 
 
-
-  const getRandomPeople = async()=> {
+  const getUserData = async()=> {
+    setLoading(true)
 
     try {
        const response =  await axios.get(BASE_URL)
@@ -30,61 +33,92 @@ function App() {
        if(response.status !== 200) {
          throw new Error("Data is not fetched")
         }
-        SetPeople(response.data.results)
+        setPeople(response.data.results)
+        setLoading(false)
     } catch (error) {
       console.log(error);
     }
  
-
-  
 }
   
   useEffect(()=>{
    
-    getRandomPeople()
+    getUserData()
   },[])
 
   console.log(people);
+
+const addNewUser = (e) => {
+  console.log(e);
+
+}
+
+
 
 
   return (
     <main>
    
-{people.map((person)=>(
+{people && people.map((person)=>(
   
   <div key={person.id} className="block">  
   <div className="block bcg-orange">
         <img src={logo} alt="logo" id="logo" />
       </div>
         <div className="container">
-          <img src={person.picture.medium} alt="random user" className="user-img" />
-          <p className="user-title">My ... is</p>
-          <p className="user-value">{person.name.first} {person.name.last}</p>
+          <img src= {person.picture.medium} alt="random user" className="user-img" />
+          <p className="user-title">{`My name is`}</p>
+      <p className="user-value">{person.name.first} {person.name.last}</p>
           <div className="values-list">
             <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" />
+          <img src={person.gender === "male" ? manSvg : womanSvg} alt="user" id="iconImg" 
+            onMouseOver={() => {
+  
+            }}
+          />
             </button>
-            <button className="icon" data-label="email">
+            <button className="icon" data-label="email"
+            onMouseOver={()=>{
+            
+            }}
+            >
               <img src={mailSvg} alt="mail" id="iconImg" />
             </button>
-            <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
+            <button className="icon" data-label="age"
+          onMouseOver={() => {
+          
+          }}
+            >
+              <img src={person.gender === "male" ? manAgeSvg : womanAgeSvg} alt="age" id="iconImg" />
             </button>
-            <button className="icon" data-label="street">
+            <button className="icon" data-label="street"
+          onMouseOver={() => {
+          
+          }}>
               <img src={mapSvg} alt="map" id="iconImg" />
             </button>
-            <button className="icon" data-label="phone">
+            <button className="icon" data-label="phone"
+          onMouseOver={() => {
+           
+          }}
+            >
               <img src={phoneSvg} alt="phone" id="iconImg" />
             </button>
-            <button className="icon" data-label="password">
+            <button className="icon" data-label="password"
+          onMouseOver={() => {
+          
+          }}
+            >
               <img src={padlockSvg} alt="lock" id="iconImg" />
             </button>
           </div>
           <div className="btn-group">
-            <button onClick={()=>getRandomPeople()} className="btn" type="button">
+        {loading ? <button className="btn" type="button">
+          loading...
+        </button> : <button onClick={()=>getUserData()} className="btn" type="button">
               new user
-            </button>
-            <button className="btn" type="button">
+            </button>}  
+            <button onClick={(e)=> addNewUser()} className="btn" type="button">
               add user
             </button>
           </div>
